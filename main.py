b9b099075386884cbd144a3da0b753776e689a8a
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2022-02-27 08:22:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,19 @@ import discord
 from discord.ext import commands
 import asyncio
 from datetime import datetime
-import moderator
+import moderator #cog moderator.py
 import pytz
 
-client = commands.Bot(command_prefix='**', intents=discord.Intents.all())
+client = commands.Bot(command_prefix='prefix disini', intents=discord.Intents.all()) #prefix nya terserah
 
-cogs = [moderator]
+cogs = [moderator] #kalian bisa tambahin cogs yang sudah kalian buat (Harus di import terlebih dahulu)
 
 for i in range(len(cogs)):
-    cogs[i].setup(client)
-
-whitelist = [836464932236165140, 851745883825373225]
+    cogs[i].setup(client) #Method setup yang ada di dalam Cogs
 
 @client.event
 async def on_ready():
     print('Bot Online')
 
-@client.event
-async def on_guild_join(guild):
-    if guild.id not in whitelist:
-        await guild.owner.send('Your Server Blacklisted!!')
-        await guild.leave()
-
-client.run('OTMwNzg1OTQ5NzgzMjk4MDY4.Yd679w.Sq_ba2UrICO_G3w5vCrKejvv0ZY')
+client.run(os.environ.get('TOKEN'))
+#client.run(os.getenv('TOKEN'))
