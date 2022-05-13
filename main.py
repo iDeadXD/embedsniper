5b2d57ca282f41e05b5e6b17ff01a5256408ed31
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2022-05-13 20:03:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
-import discord
+import discord #pip install discord.py (pakai discord.py 1.7.3)
 from discord.ext import commands
 import asyncio
 from datetime import datetime
-import moderator #cog moderator.py
+import embed #Cog embed.py
 import pytz
 
-client = commands.Bot(command_prefix='prefix disini', intents=discord.Intents.all()) #prefix nya terserah
+client = commands.Bot(command_prefix='prefix disini', intents=discord.Intents.default()) #prefix nya terserah
 
-cogs = [moderator] #kalian bisa tambahin cogs yang sudah kalian buat (Harus di import terlebih dahulu)
+cogs = [embed] #kalian bisa tambahin cogs yang sudah kalian buat (Harus di import terlebih dahulu)
 
 for i in range(len(cogs)):
     cogs[i].setup(client) #Method setup yang ada di dalam Cogs
@@ -17,5 +17,6 @@ for i in range(len(cogs)):
 async def on_ready():
     print('Bot Online')
 
-client.run(os.environ.get('TOKEN'))
+client.run('TOKEN')
 #client.run(os.getenv('TOKEN'))
+#client.run('Langsung Token')
