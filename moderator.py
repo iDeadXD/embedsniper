4d553433166d49ad986cb41b6c2d836c965a63f5
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2022-05-13 19:58:37 +0700

    Update moderator.py

--- moderator.py
+++ moderator.py
@@ -10,12 +10,12 @@ dataclient = MongoClient(CONFIG['mongodb_url']) #Edit mongodb_url nya di config.
 database = dataclient['nama database'] #Sesuaikan dengan nama database yang kalian buat
 saved = database['collection'] #Sesuaikan dengan nama collection pada saat kalian membuat database nya
 
-class Moderator(commands.Cog):
+class EmbedSniper(commands.Cog):
     def __init__(self, client):
         self.client = client
     
-    @commands.Cog.listener() #Event listener di dalam Cogs
-    async def on_message_delete(self, message):
+    @commands.Cog.listener() #Event listener di dalam Cog
+    async def on_message_delete(self, message: discord.Message): #Menggunakkan event on_message_delete untuk mendapatkan message yang dihapus
         data = saved.find_one({'_id': message.channel.id}) #Find document dari MongoDB dengan filter id channel saat ini 
         
         #Cek: Kalo message nya dalam bentuk embed
@@ -25,23 +25,23 @@ class Moderator(commands.Cog):
                 now = datetime.now(pytz.timezone('Asia/Jakarta')) #
                 
                 #Data embed yang bakal di ambil
-                embed = message.embeds[0]
-                embtitle = embed.title
-                embdescription = embed.description
-                embfooter = embed.footer.text
-                embfootericon = embed.footer.icon_url
-                embfields = embed.fields
-                embimage = embed.image.url
-                embthumbnail = embed.thumbnail.url
+                embed = message.embeds[0] #Embed
+                embtitle = embed.title #Embed title
+                embdescription = embed.description #Embed description
+                embfooter = embed.footer.text #Embed footer text
+                embfootericon = embed.footer.icon_url #Embed footer icon url
+                embfields = embed.fields #Embed total fields
+                embimage = embed.image.url #Embed image url
+                embthumbnail = embed.thumbnail.url #Embed thumbnail url
                 
                 #Kode yang berantakan
-                title = 'None' if len(embtitle) == 0 else embtitle
-                description = 'None' if len(embdescription) == 0 else embdescription
-                footer = 'None' if len(embfooter) == 0 else embfooter
-                footer_icon = 'None' if len(embfootericon) == 0 else embfootericon
-                image = 'None' if len(embimage) == 0 else embimage
-                fields = 'None' if len(embfields) == 0 else str(len(embfields))
-                thumbnail = 'None' if len(embthumbnail) == 0 else embthumbnail
+                title = None or embtitle
+                description = None or embdescription
+                footer = None or embfooter
+                footer_icon = None or embfootericon
+                image = None or embimage
+                fields = None or str(len(embfields))
+                thumbnail = None or embthumbnail
                 
                 #Cek: Kalo baru ada embed dihapus di channel saat ini atau sebaliknya
                 if data is None:
@@ -52,11 +52,11 @@ class Moderator(commands.Cog):
                     #Mengupdate data yang telah tersedia
                     saved.update_one({'_id': message.channel.id}, {'$set': {'title': f'{title}', 'description': f'{description}', 'fields': f'{fields}', 'footer': f'{footer}', 'footer_icon': f'{footer_icon}', 'image': f'{image}', 'thumbnail': f'{thumbnail}', 'author': message.author.id}})
                     await dev.send(f'Embed Data has been Updated!\nTimestamp: {now}\nServer: {message.guild.name}\nChannel: {message.channel.mention}\n-----------------------') #Send message ke Dev (Optional)
-            except Exception as e:
+            except Exception as e: #Exception jika terjadi kesalahan
                 return print(e)
     
-    @commands.command(aliases=['se']) #Command di dalam Cogs
-    async def snipe_embed(self, ctx):
+    @commands.command(aliases=['se']) #Command di dalam cog
+    async def snipe_embed(self, ctx: commands.Context):
         data = saved.find_one({'_id': ctx.channel.id}) #Find document dari MongoDB dengan filter id channel saat ini
         
         #Cek: Kalo ngga ada embed yang dihapus di channel saat ini
@@ -68,25 +68,25 @@ class Moderator(commands.Cog):
             )
             return await ctx.send(embed=failed) #Bakal send error message
         
-        author = self.client.get_user(data['author']) #Get user/author dari deleted embed
-        recreate = discord.Embed(
+        author = self.client.get_user(data['author']) #Get user/author dari embed yang terhapus
+        result = discord.Embed(
             title='--- Embed Sniped! ---',
             description=f'Deleted Embed in {ctx.channel.mention}',
             color=discord.Color.purple(),
             timestamp=ctx.message.created_at
         )
-        recreate.add_field(name='Title', value=f'{data["title"]}')
-        recreate.add_field(name='Description', value=f'{data["description"]}')
-        recreate.add_field(name='Footer', value=f'{data["footer"]}')
-        recreate.add_field(name='Total Fields', value=f'{data["fields"]}')
-        recreate.add_field(name='Footer Icon URL', value=f'{data["footer_icon"]}')
-        recreate.add_field(name='Thumbnail URL', value=f'{data["thumbnail"]}')
-        recreate.add_field(name='Image URL', value=f'{data["image"]}')
-        recreate.add_field(name='Author', value=f'{author.name + "#" + author.discriminator} / {author.mention}')
-        recreate.set_footer(text=f'Sniped by {ctx.author.name + "#" + ctx.author.discriminator}')
+        result.add_field(name='Title', value=f'{data["title"]}')
+        result.add_field(name='Description', value=f'{data["description"]}')
+        result.add_field(name='Footer', value=f'{data["footer"]}')
+        result.add_field(name='Total Fields', value=f'{data["fields"]}')
+        result.add_field(name='Footer Icon URL', value=f'{data["footer_icon"]}')
+        result.add_field(name='Thumbnail URL', value=f'{data["thumbnail"]}')
+        result.add_field(name='Image URL', value=f'{data["image"]}')
+        result.add_field(name='Author', value=f'{author.name + "#" + author.discriminator} / {author.mention}')
+        result.set_footer(text=f'Sniped by {ctx.author.name + "#" + ctx.author.discriminator}')
         
-        await ctx.send(embed=recreate) #Send embed berisi data embed yang sebelumnya terhapus
+        await ctx.send(embed=result) #Send embed berisi data embed yang sebelumnya terhapus
 
 #Setup
 def setup(client):
-    client.add_cog(Moderator(client))
+    client.add_cog(EmbedSniper(client))
